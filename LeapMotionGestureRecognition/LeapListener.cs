2c8c6453c624c0c1cf51620fc0ab4a5c6a285773
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-10-02 21:11:28 +0200

    Add project files.

---
+++ LeapMotionGestureRecognition/LeapListener.cs
@@ -0,0 +1,87 @@
+﻿using Leap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapMotionGestureRecognition
+{
+    public class LeapListener : Listener
+    {
+        //fields
+        private GestureList gestures = null;
+        private HandList hands = null;
+        private Vector pointerPosition = null;
+        private Object thisLock = new Object();
+
+
+        //constructor
+        public LeapListener()
+        {
+        }
+
+        
+
+        private void SafeWriteLine(String line)
+        {
+            lock (thisLock)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public override void OnConnect(Controller controller)
+        {
+            Console.WriteLine("Connected");
+            controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+            controller.EnableGesture(Leap.Gesture.GestureType.TYPE_CIRCLE);
+            controller.EnableGesture(Leap.Gesture.GestureType.TYPE_SCREEN_TAP);
+
+        }
+
+        public override void OnFrame(Controller controller)
+        {
+            Frame frame = controller.Frame();
+
+            InteractionBox interactionBox = frame.InteractionBox;
+            pointerPosition = interactionBox.NormalizePoint(frame.Pointables.Frontmost.TipPosition);
+            hands = frame.Hands;
+            gestures = frame.Gestures();
+
+            if (hands.Count.Equals(1))
+            {
+                string handName = hands[0].IsLeft ? "Left Hand" : "Right Hand";
+                GetGesture(handName);
+            } else if (hands.Count.Equals(2)){
+                Print("Both hands up!");
+            }                 
+        }
+
+        private void GetGesture(string handName)
+        {
+            foreach (Gesture gesture in gestures)
+            {
+                if (gesture.State.Equals(Gesture.GestureState.STATESTOP))
+                {
+                    if (gesture.Type.Equals(Gesture.GestureType.TYPE_SCREEN_TAP))
+                    {
+                        Print("Screen Tap Detected With " + handName);
+                    }
+                    if (gesture.Type.Equals(Gesture.GestureType.TYPE_SWIPE))
+                    {
+                        Print("Swipe Detected With " + handName);
+                    }
+                }
+            }
+        }
+
+        private void Print(String output)
+        {
+            lock (thisLock)
+            {
+                Console.WriteLine("[OUTPUT] " + output);
+            }
+        }
+    }
+}
---
+++ LeapMotionGestureRecognition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+
+namespace LeapMotionGestureRecognition
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //create listener and controller
+            Controller controller = new Controller();
+            LeapListener listener = new LeapListener();
+            controller.AddListener(listener);
+
+            // Keep this process running until Enter is pressed
+            Console.WriteLine("Press Enter to quit...");
+            Console.ReadLine();
+
+            controller.RemoveListener(listener);
+            controller.Dispose();
+        }
+
+
+    }
+}
